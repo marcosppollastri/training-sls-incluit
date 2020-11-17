ff52951da517c6ea47497e8f267ef6826fb2fdd2
Hernan Giayetto <hernangiayetto@gmail.com>
2020-11-16 21:44:32 -0300

    improvements

--- handler.js
+++ handler.js
@@ -1,3 +1,15 @@
-module.exports.lookAtThisLambda = async(event) => {
-  return { statusCode: 200 };
+
+const AN_ENV_VAR = process.env.AN_ENV_VAR || '';
+
+async function lookAtThisLambda(event) {
+  console.log(`You've called this owesome lambda with this body ${event.body}`);
+  console.log(`This is a local var ${AN_ENV_VAR}`);
+  return {
+    statusCode: 200,
+    body: {
+      success: true,
+    },
+  };
 };
+
+module.exports = { lookAtThisLambda };
